8eb7e4c429b65915782d9fd4f55441f7573c55d9
0385246651 <haibaba2k@gmail.com>
2025-08-28 10:05:04 +0700

    Controllers-Views-Models

--- Program.cs
+++ Program.cs
@@ -1,7 +1,30 @@
+using App.Services;
+using Microsoft.AspNetCore.Mvc.Razor;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// có thể cài thêm logging provider khác như Serilog, NLog, ...
+// services.AddTransient(typeof(ILogger<>), typeof(Logger<>)); //Serilog
+
+// Cấu hình để tìm view trong thư mục MyView
+builder.Services.Configure<RazorViewEngineOptions>(options =>
+{
+    // /View/Controller/Action.cshtml
+    // /MyView/Controller/Action.cshtml
+
+    // {0} -> ten Action
+    // {1} -> ten Controller
+    // {2} -> ten Area
+    options.ViewLocationFormats.Add("/MyView/{1}/{0}" + RazorViewEngine.ViewExtension);
+});
+
+//cấu hình services
+// services.AddSingleton<ProductService>();
+// services.AddSingleton<ProductService, ProductService>();
+// services.AddSingleton(typeof(ProductService));
+builder.Services.AddSingleton(typeof(ProductService), typeof(ProductService));
 
 var app = builder.Build();
 
@@ -16,14 +39,18 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
-app.UseAuthorization();
+app.UseAuthentication(); // xac dinh danh tinh 
+app.UseAuthorization();  // xac thuc  quyen truy  cap
 
 app.MapStaticAssets();
+
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+
+app.MapRazorPages();
 
 app.Run();
