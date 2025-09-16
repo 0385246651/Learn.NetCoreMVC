f61156da1dc5d5fcf44351431c267238d6bccae0
0385246651 <haibaba2k@gmail.com>
2025-09-16 15:22:28 +0700

    feat: done setup indentity

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Net;
 using App.ExtendMethods;
 using App.Models;
 using App.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.Routing.Constraints;
@@ -41,6 +42,68 @@ builder.Services.Configure<RazorViewEngineOptions>(options =>
 });
 
 builder.Services.AddSingleton<PlanetService>();
+
+//Đăng ký dịch vụ Identity
+builder.Services.AddIdentity<AppUser, IdentityRole>() //sử dụng lớp AppUser và Role mặc định
+    .AddEntityFrameworkStores<AppDbContext>() //sử dụng EF để lưu trữ thông tin user, role
+    .AddDefaultTokenProviders(); //sinh token mặc định (dùng cho xác thực email, quên mật khẩu, ...)
+
+// Truy cập IdentityOptions
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Thiết lập về Password
+    options.Password.RequireDigit = false; // Không bắt phải có số
+    options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+    options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+    options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+    options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+    options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
+
+    // Cấu hình Lockout - khóa user
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); // Khóa 5 phút
+    options.Lockout.MaxFailedAccessAttempts = 3; // Thất bại 3 lầ thì khóa
+    options.Lockout.AllowedForNewUsers = true;
+
+    // Cấu hình về User.
+    options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+    options.User.RequireUniqueEmail = true;  // Email là duy nhất
+
+
+    // Cấu hình đăng nhập.
+    options.SignIn.RequireConfirmedEmail = true;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+    options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
+    options.SignIn.RequireConfirmedAccount = true;
+
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/login/";
+    options.LogoutPath = "/logout/";
+    options.AccessDeniedPath = "/khongduoctruycap.html";
+});
+
+builder.Services.AddAuthentication()
+        .AddGoogle(options =>
+        {
+            var gconfig = builder.Configuration.GetSection("Authentication:Google");
+            options.ClientId = gconfig["ClientId"];
+            options.ClientSecret = gconfig["ClientSecret"];
+            // https://localhost:5001/signin-google
+            options.CallbackPath = "/dang-nhap-tu-google";
+        })
+        .AddFacebook(options =>
+        {
+            var fconfig = builder.Configuration.GetSection("Authentication:Facebook");
+            options.AppId = fconfig["AppId"];
+            options.AppSecret = fconfig["AppSecret"];
+            options.CallbackPath = "/dang-nhap-tu-facebook";
+        })
+        // .AddTwitter()
+        // .AddMicrosoftAccount()
+        ;
+
 
 //cấu hình services
 // services.AddSingleton<ProductService>();
